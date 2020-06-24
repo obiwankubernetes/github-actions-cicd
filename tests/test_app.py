71cdcda7fe39c7a74cd1a4aa6462b2c5dd22e64b
obiwankubernetes <samrperl@gmail.com>
2020-06-24 12:54:43 -0700

    prepare heroku deploy

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,7 +1,7 @@
 # unit testing app.py
 
 from app import index
-# need to set PYTHONPATH=src to get index from src/app.py module
+# locally, need to set PYTHONPATH=src to get index from src/app.py module
 
 def test_index():
     assert index() == "Hello World -- GitHub Actions is Awesome!"
